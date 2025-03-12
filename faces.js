eb45d6a1f112ee6559baae201b0d27284150ec17
o-uki <makaroniaji024@gmail.com>
2025-03-13 00:57:46 +0900

    [update] faces.js sample.faces

--- faces.js
+++ faces.js
@@ -4,9 +4,41 @@ const fs = require("fs");
 
 // Fa(c_e)Script実行関数
 const faces = (file) => {
+    // ファイル読み込み
     if (path.extname(file) === ".faces") {
+        //　ソースコード
         const sourceCode = fs.readFileSync(file, "utf-8");
-        console.log(sourceCode);
+
+        //　トークンの配列
+        const tokens = [
+            ["void", " "],
+            ["void", "\r"],
+            ["void", "\n"],
+            ["parenthesisLeft", "(>_)⅃"],
+            ["parenthesisRight", "L(_<)"],
+            ["binaryZero", "(-_-)"],
+            ["binaryOne", "(o_o)"],
+            ["print", "('O')"]
+        ]
+
+        // 字句解析関数
+        const lexical = (sourceCode) => {
+            const sourceCodeTokens = [];
+
+            for (let i = 0; i < sourceCode.length; i++) {
+                for (let j = 0; j < tokens.length; j++) {
+                    if (sourceCode.substr(i, tokens[j][1].length) === tokens[j][1]) {
+                        if (!(tokens[j][0] === "void")) {
+                            sourceCodeTokens.push(tokens[j]);
+                        }
+
+                        i = i + tokens[j][1].length - 1;
+                    }
+                }
+            }
+
+            return sourceCodeTokens;
+        }
     }
 }
 
