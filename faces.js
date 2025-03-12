099ea34a9f8ab582c5948c02a460a630fc063b5b
o-uki <makaroniaji024@gmail.com>
2025-03-13 00:08:13 +0900

    [update] faces.js sample.faces

--- faces.js
+++ faces.js
@@ -1,2 +1,13 @@
-// ソースコード
-const sourceCode = "";
+// モジュール読み込み
+const path = require("path");
+const fs = require("fs");
+
+// Fa(c_e)Script実行関数
+const faces = (file) => {
+    if (path.extname(file) === ".faces") {
+        const sourceCode = fs.readFileSync(file, "utf-8");
+        console.log(sourceCode);
+    }
+}
+
+faces("sample.faces");
