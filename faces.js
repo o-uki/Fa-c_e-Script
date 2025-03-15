12d6f3348c05864f98c986175c67f53e0b12e601
o-uki <makaroniaji024@gmail.com>
2025-03-16 00:53:48 +0900

    [update] faces.js sample.faces

--- faces.js
+++ faces.js
@@ -18,30 +18,118 @@ const faces = (file) => {
             ["binaryOne", "(o_o)"],
             ["end", "L(-.<)"],
             ["separate", "⊂(¯^¯)⊃"],
+            ["add", "(>ω<)⊃"],
+            ["minus", "('^;)⊃"],
+            ["absolute", "(O∇O)⊃"],
             ["print", "('O')⅃"]
         ];
 
-        // 字句解析関数
-        const lexical = (sourceCode) => {
-            const sourceCodeTokens = [];
+        // 演算子を認識して演算する関数
+        const operators = [
+            ["add", 2, (operands) => {
+                return operands[0] + operands[1];
+            }],
+            ["absolute", 1, (operands) => {
+                return Math.abs(operands[0]);
+            }],
+            ["minus", 1, (operands) => {
+                return -operands[0];
+            }]
+        ];
 
-            for (let i = 0; i < sourceCode.length; i++) {
-                for (let j = 0; j < tokens.length; j++) {
-                    if (sourceCode.substr(i, tokens[j][1].length) === tokens[j][1]) {
-                        if (!(tokens[j][0] === "void")) {
-                            sourceCodeTokens.push(tokens[j][0]);
+        const operateData = (operator) => {
+            for (let i = 0; i < operators.length; i++) {
+                if (operator === operators[i][0]) {
+                    return [operators[i][1], operators[i][2]];
+                }
+            }
+        };
+
+        // 字句解析してトークンの名前を並べる
+        const tokenNames = [];
+
+        for (let i = 0; i < sourceCode.length; i++) {
+            for (let j = 0; j < tokens.length; j++) {
+                if (sourceCode.substring(i, i + tokens[j][1].length) === tokens[j][1]) {
+                    if (!(tokens[j][0] === "void")) {
+                        tokenNames.push(tokens[j][0]);
+                    }
+
+                    i = i + tokens[j][1].length - 1;
+                }
+            }
+        }
+
+        // 値を数字に変換して引数として並べる
+        let commands = [];
+
+        (() => {
+            let commandArgument = {
+                command: "",
+                arguments: []
+            };
+            let binaryBits = [];
+            
+            let operatorIndex = [null, null];
+            let operate = null;
+
+            // 演算子があると演算する
+            const operatedValueGet = () => {
+                const operatedValue = (() => {
+                    if (operatorIndex[1] != null) {
+                        operatorIndex[0]--;
+                        if (operatorIndex[0] === 0) {
+                            let operands = [];
+    
+                            for (let i = 0; i < operatorIndex[1]; i++) {
+                                operands.push(commands[commands.length - 1].arguments.pop());
+                            }
+    
+                            return operate(operands);
                         }
+                    }
+                })();
 
-                        i = i + tokens[j][1].length - 1;
-                        break;
+                if (operatedValue != undefined) {
+                    commands[commands.length - 1].arguments.push(operatedValue);
+
+                    operatorIndex = [null, null];
+                    operate = null;
+                }
+            };
+
+            for (let i = 0; i < tokenNames.length; i++) {
+                if (commandArgument.command === "") {
+                    commandArgument.command = tokenNames[i];
+                    commands.push(structuredClone(commandArgument));
+                } else {
+                    if (tokenNames[i] === "binaryZero") { // 二進数を数値に変換
+                        binaryBits.push("0");
+                    } else if (tokenNames[i] === "binaryOne") {
+                        binaryBits.push("1");
+                    } else { // 演算子と区切りと終了を処理
+                        if (tokenNames[i] === "end") {
+                            commands.slice(-1)[0].arguments.push(Number("0b" + binaryBits.join("")));
+                            binaryBits = [];
+                            commandArgument.command = "";
+
+                            operatedValueGet();
+                        } else if (tokenNames[i] != "separate") {
+                            operatorIndex[0] = operateData(tokenNames[i])[0];
+                            operatorIndex[1] = operateData(tokenNames[i])[0];
+                            operate = operateData(tokenNames[i])[1];
+                        } else {
+                            commands.slice(-1)[0].arguments.push(Number("0b" + binaryBits.join("")));
+                            binaryBits = [];
+                            
+                            operatedValueGet();
+                        }
                     }
                 }
             }
+        })();
 
-            return sourceCodeTokens;
-        };
-
-        console.log(lexical(sourceCode));
+        console.log(commands);
     }
 };
 
